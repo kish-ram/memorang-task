c595af2137fbf668865c4f3f804bef4d6153d9fc
gb_kishore <kishore.ram@globalbees.com>
2021-11-08 05:03:40 +0530

    logout added

--- mmdb-dashboard/components/Nav.js
+++ mmdb-dashboard/components/Nav.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import styles from "../styles/Nav.module.css";
 import {
   Navbar,
@@ -7,11 +8,21 @@ import {
 } from "react-bootstrap";
 
 const TopNavBar = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    if(localStorage.getItem('memorang-email')){
+      setLoggedIn(true);
+    }
+  },[])
+  console.log(isLoggedIn)
   return (
      <Navbar bg="dark" variant="dark">
      <Container>
      <Nav className="me-auto">
-     <Link href="/login" passHref><Nav.Link>Login</Nav.Link></Link>
+      {isLoggedIn 
+        ? <Link href="/logout" passHref><Nav.Link>Logout</Nav.Link></Link> 
+        : <Link href="/login" passHref><Nav.Link>Login</Nav.Link></Link> }
+     
      <Link href="/search" passHref><Nav.Link>Search</Nav.Link></Link>
      <Link href="/favorites" passHref><Nav.Link>Favorites</Nav.Link></Link>
      </Nav>
