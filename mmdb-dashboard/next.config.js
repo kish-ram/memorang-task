411b004badcde2b93d8c744e9c56dc56bae0ffdf
gb_kishore <kishore.ram@globalbees.com>
2021-11-08 05:35:00 +0530

    updated redirects in next.config.js

--- mmdb-dashboard/next.config.js
+++ mmdb-dashboard/next.config.js
@@ -2,4 +2,13 @@ module.exports = {
     images: {
       domains: ['image.tmdb.org'],
     },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/index',
+          permanent: true,
+        },
+      ]
+    },
   }
